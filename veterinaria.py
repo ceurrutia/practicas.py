7a15f7cc495fbfec1e2f8707f0ba23d2f0d65210
Cecilia Urrutia <urrutia.cici@gmail.com>
2023-06-06 19:18:44 -0300

    Muestra listado

--- veterinaria.py
+++ veterinaria.py
@@ -3,19 +3,20 @@
 class Mascota():
     #inicializo constructor
     
-    def __init__(self, nombre, especie, animal, raza, nombre_duenio):
+    def __init__(self, nombre, edad, animal, propietario):
         #Atributos mascota
         self.nombre = nombre
-        self.especie = especie
+        self.edad = edad
         self.animal = animal
-        self.raza = raza
-        self.nombre_duenio = nombre_duenio
+        self.propietario = propietario
+        
+        
 
         #Creo el método string
 
         def __str__(self)-> str:
     
-         return f'{self.nombre}, {self.especie}, {self.animal}, {self.raza}, {self.nombre_duenio}'
+         return f'{self.nombre}, {self.edad}, {self.animal}, {self.propietario}'
 
 #creo la lista
 i = 0
@@ -25,37 +26,37 @@ lista = []
 def sistema():
     k=0
     while k < len(lista):
-        print(lista[k].nombre, "", lista[k].especie, "", lista[k].animal, "",lista[k].raza, "", lista([k].nombre_duenio))
-        k+=1
-
+        print(lista[k].nombre, "", lista[k].edad, "", lista[k].animal, "",lista[k].propietario,"" )
+        k +=1
 while i ==0:
     print("Menu - Seleccione la opción ")
     print("1. Registrar mascota: ")
     print("2. Consultar listado: ")
     print("3. Salir")
     opcion = int(input())
-    if opcion==1:
+    if opcion == 1:
         print("Registrar mascota") 
        
         #inicio variables de entrada
        
         n = input("Ingrese nombre de la mascota: ")
-        e = input("Ingrese la especie de la mascota: (Canino, felino, ave, mustélido, etc): ")
+        e = input("Ingrese la edad de la mascota: ")
         a = input("Ingrese el tipo de animal (Gato, perro, rana de felpa, hurón, loro): ")
-        r = input("Ingrese la raza del animalito: ")
-        d = input("Ingrese nombre del dueño de la mascota: ")
+        p = input("Ingrese el nombre del propietario del animalito: ")
+        
        
         #Guardo los datos de entrada
        
-        masc = Mascota(n, e, a, r, d)
-        lista.append(masc)
+        masc = Mascota(n, e, a, p)
+        lista.append(masc)  
         
-        
+           
         print("Mascota registrada con éxito! ")
-        print("Ha registrado a: ", n, "Especie: ",e, "Es de tipo: ",a , "Raza: ",r, "Su dueño/a es: ",d )
+        print("Ha registrado a: ", n, "Edad: ",e, "Es de tipo: ",a , "Propietario: ",p )
 
     elif opcion == 2:
-        print("Consulta listado ")
+        print("Consulta de animales: ")
+        sistema()
         
     elif opcion == 3:
         exit()
