43c14e1e64632de57e5fae5b36e6517f7f1b6c1b
Cecilia Urrutia <urrutia.cici@gmail.com>
2023-06-06 20:12:20 -0300

    Mejoro nombres de variables

--- veterinaria.py
+++ veterinaria.py
@@ -1,17 +1,14 @@
 #defino la clase mascota
 
 class Mascota():
-    #inicializo constructor
-    
+    #inicializo constructor    
     def __init__(self, nombre, edad, animal, propietario):
         #Atributos mascota
         self.nombre = nombre
         self.edad = edad
         self.animal = animal
         self.propietario = propietario
-        
-        
-
+         
         #Creo el método string
 
         def __str__(self)-> str:
@@ -26,7 +23,7 @@ lista = []
 def sistema():
     k=0
     while k < len(lista):
-        print(lista[k].nombre, "", lista[k].edad, "", lista[k].animal, "",lista[k].propietario,"" )
+        print(lista[k].nombre, " ", lista[k].edad, " ", lista[k].animal, " ", lista[k].propietario," " )
         k +=1
 while i ==0:
     print("Menu - Seleccione la opción ")
@@ -39,24 +36,25 @@ while i ==0:
        
         #inicio variables de entrada
        
-        n = input("Ingrese nombre de la mascota: ")
-        e = input("Ingrese la edad de la mascota: ")
-        a = input("Ingrese el tipo de animal (Gato, perro, rana de felpa, hurón, loro): ")
-        p = input("Ingrese el nombre del propietario del animalito: ")
+        nom = input("Ingrese nombre de la mascota: ")
+        ed = input("Ingrese la edad de la mascota: ")
+        ani = input("Ingrese el tipo de animal (Gato, perro, rana de felpa, hurón, loro): ")
+        prop = input("Ingrese el nombre del propietario del animalito: ")
         
        
         #Guardo los datos de entrada
        
-        masc = Mascota(n, e, a, p)
+        masc = Mascota(nom, ed, ani, prop)
         lista.append(masc)  
         
-           
+        #Imprimo en consola
+         
         print("Mascota registrada con éxito! ")
-        print("Ha registrado a: ", n, "Edad: ",e, "Es de tipo: ",a , "Propietario: ",p )
+        print("Ha registrado a: ", nom, "Edad: ",ed, "Es de tipo: ",ani , "Propietario: ",prop )
 
     elif opcion == 2:
         print("Consulta de animales: ")
-        sistema()
+        sistema() #Trae la lista de mascotas en el sistema
         
     elif opcion == 3:
         exit()
