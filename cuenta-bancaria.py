df8458f4ab79ac410a6ef27a32e2eccdf4c99e58
Cecilia Urrutia <urrutia.cici@gmail.com>
2023-06-06 15:06:25 -0300

    corrijo set

--- cuenta-bancaria.py
+++ cuenta-bancaria.py
@@ -6,7 +6,7 @@ class Cuenta():
         def __init__(self, propietario, saldo, moneda):
             self.propietario = propietario
             self.saldo = saldo
-            self.moneda = "pesos"
+            self.moneda = "Pesos"
 
         #Invoco metodo get (leer propiedades de una clase que estan encapsuladas)
 
@@ -16,7 +16,7 @@ class Cuenta():
         def get_propietario(self):
               return self.propietario
         
-        def get_moenda(self):
+        def get_moneda(self):
               return self.moneda
         
         #Invoco metodo set (modificar propiedades de una clase por ejemplo pesos por dolares)
@@ -24,9 +24,9 @@ class Cuenta():
         def set_moneda(self, moneda):
               self.moneda = moneda
 
-cuenta1 = Cuenta("Cecilia urrutia", 3000, "Peso Argentino")
+cuenta1 = Cuenta("Cecilia urrutia", 3000, "Pesos")
 
 print(cuenta1.get_saldo())
 print(cuenta1.get_moneda())
-cuenta1.set_moneda("Dolares")
+print(cuenta1.set_moneda("Dolares"))
 print(cuenta1.get_moneda())
