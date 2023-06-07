365729e55802215c3b51d0e32134226f7d32e20a
Cecilia Urrutia <urrutia.cici@gmail.com>
2023-06-07 13:47:39 -0300

    Sumo opcion 4 valido str

--- veterinaria.py
+++ veterinaria.py
@@ -1,72 +1,78 @@
 #defino la clase mascota
-
 class Mascota():
-    #inicializo constructor    
-    def __init__(self, nombre, edad, animal, propietario):
-        #Atributos mascota
-        self.nombre = nombre
-        self.edad = edad
-        self.animal = animal
-        self.propietario = propietario
-         
-        #Creo el método string
+  #inicializo constructor
+  def __init__(self, nombre, edad, animal, propietario, telefono):
+    #Atributos mascota
+    self.nombre = nombre
+    self.edad = edad
+    self.animal = animal
+    self.propietario = propietario
+    self.telefono = telefono
 
-        def __str__(self)-> str:
-    
-         return f'{self.nombre}, {self.edad}, {self.animal}, {self.propietario}'
+    #Creo el método string
+
+    def __str__(self) -> str:
+
+      return f'{self.nombre}, {self.edad}, {self.animal}, {self.propietario}, {self.telefono}'
+
 
 #creo la lista
 i = 0
 lista = []
 
+agrega_mascota = True
+
 #Función sistema muestra los animales registrados
 def sistema():
-    k=0
-    while k < len(lista):
-        print(lista[k].nombre, " ", lista[k].edad, " ", lista[k].animal, " ", lista[k].propietario," " )
-        k +=1
-while i ==0:
-    print("Menu - Seleccione la opción ")
-    print("1. Registrar mascota: ")
-    print("2. Consultar listado: ")
-    print("3. Salir")
-    opcion = int(input())
-    if opcion == 1:
-        print("Registrar mascota") 
-       
-        #inicio variables de entrada
-       
-        nom = input("Ingrese nombre de la mascota: ")
-        ed = input("Ingrese la edad de la mascota: ")
-        ani = input("Ingrese el tipo de animal (Gato, perro, rana de felpa, hurón, loro): ")
-        prop = input("Ingrese el nombre del propietario del animalito: ")
-        
-       
-        #Guardo los datos de entrada
-       
-        masc = Mascota(nom, ed, ani, prop)
-        lista.append(masc)  
-        
-        #Imprimo en consola
-         
-        print("Mascota registrada con éxito! ")
-        print("Ha registrado a: ", nom, "Edad: ",ed, "Es de tipo: ",ani , "Propietario: ",prop )
+  k = 0
+  while k < len(lista):
+    print(lista[k].nombre, " ", lista[k].edad, " ", lista[k].animal, " ",
+          lista[k].propietario, " ", lista[k].telefono)
+    k += 1
 
-    elif opcion == 2:
-        print("Consulta de animales: ")
-        sistema() #Trae la lista de mascotas en el sistema
-        
-    elif opcion == 3:
+
+while i == 0:
+  print("Menu - Seleccione la opción ")
+  print("1. Registrar mascota: ")
+  print("2. Agregar una nueva mascota: ")
+  print("3. Consultar listado: ")
+  print("4. Salir")
+  opcion = int(input())
+  if opcion == 1:
+    print("Registrar mascota")
+
+    #inicio variables de entrada
+
+    nom = input("Ingrese nombre de la mascota: ")
+    ed = input("Ingrese la edad de la mascota: ")
+    ani = input(
+      "Ingrese el tipo de animal (Gato, perro, rana de felpa, hurón, loro): ")
+    prop = input("Ingrese el nombre del propietario del animalito: ")
+    tel = input("Ingrese su teléfono de contacto: ")
+
+    #Guardo los datos de entrada
+
+    masc = Mascota(nom, ed, ani, prop, tel)
+    lista.append(masc)
+
+    print("Mascota registrada con éxito! ")
+    print("Ha registrado a: ", nom, "Edad: ", ed, "Es de tipo: ", ani,
+          "Propietario: ", prop, "Teléfono de contacto: ", tel)
+
+  elif opcion == 2:
+    opcion = input("¿Quieres agregar una nueva mascota?(si/no): ").lower()
+    while opcion != "si" and opcion != "no":
+      print("Opción no disponible, intentalo de nuevo.")
+      break
+    if opcion == "si":
+      print("Agregala ingresando el numero 1")
+    elif opcion == "no":
+      print("usted ha salido con éxito. Seleccione en el menú para más opciones")
+
+  elif opcion == 3:
+    print("Consulta de animales ingresados en el sistema: ")
+    sistema()  #Trae la lista de mascotas en el sistema
+  elif opcion == 4:
         exit()
-    else: 
-        print("Opción inválida")
-
-
-
-
-    
-    
-    
-    
-    
-    
+  else:
+    print("Opción inválida")
